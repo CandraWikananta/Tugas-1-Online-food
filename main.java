714f7625f1eb85728f6007961ef9e4ad2f6ce786
Candra Wikananta <candrawikananta@gmail.com>
2024-04-26 13:32:15 +0800

    new file:   main.class 	new file:   main.java 	new file:   systemCLS.class 	new file:   systemCLS.java     mencoba untuk membuat sistem login

---
+++ main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class main {
+
+    private static String usernameAdmin = "admin";
+    private static String passwordAdmin = "admin";
+    private static String usernameCustomer = "customer";
+    private static String passwordCustomer = "customer";
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        boolean isLoggedIn = false;
+        while(!isLoggedIn){
+            systemCLS.clearScreen();
+            System.out.println("Selamat datang di Pemesanan makanan online!");
+            System.out.println("----------------------------------------");
+            System.out.println("Silahkan Login terlebih dahulu.");
+            System.out.print("Username : ");
+            String username = scan.nextLine();
+            System.out.print("Password : ");
+            String password = scan.nextLine();
+
+            if (username.equals(usernameAdmin) && password.equals(passwordAdmin)) {
+                System.out.println("Berhasil Login sebagai Admin!");
+                isLoggedIn = true;
+                waitForInput(scan);
+                systemCLS.clearScreen();
+            }
+            else if (username.equals(usernameCustomer) && password.equals(passwordCustomer)){
+                System.out.println("Berhasil Login sebagai Customer!");
+                isLoggedIn = true;
+                waitForInput(scan);
+                systemCLS.clearScreen();
+            }
+            else{
+                System.out.println("Username dan Password salah, silahkan Login ulang!");
+                waitForInput(scan);
+                systemCLS.clearScreen();
+            }
+        }
+    }
+
+    public static void waitForInput(Scanner scan){
+        System.out.println("Tekan tombol apapun untuk lanjut");
+        scan.nextLine();
+    }
+}
